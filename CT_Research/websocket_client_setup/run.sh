009380a9de72c0c085c7696cdbd2dea7849fd842
Martin Benedikt Busch <bene.busch1992@gmail.com>
2022-12-04 21:36:03 +0100

    Only CT Research in this repo

--- CT_Research/websocket_client_setup/run.sh
+++ CT_Research/websocket_client_setup/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export HOPR_API_HOST='zero_aras_navy_amalthea.playground.hoprnet.org'
-export HOPR_API_KEY='7bac#C#8795A3aDd3f9#0e7c'
+export HOPR_API_HOST='zero_silver_fraser_io.playground.hoprnet.org'
+export HOPR_API_KEY='be7db02Ec38264e084B8f8a4#'
 
 python websocket_client.py
