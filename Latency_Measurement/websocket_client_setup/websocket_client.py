8c39f9b3b752c8926cd8183e26e847c00f531215
Lucas Benedicic <lucas.benedicic@hoprnet.org>
2022-12-15 17:54:38 +0100

    Sending message through random peer. Logging activated.-

--- Latency_Measurement/websocket_client_setup/websocket_client.py
+++ Latency_Measurement/websocket_client_setup/websocket_client.py
@@ -3,15 +3,23 @@
 # =============================================================================
 
 import json
+import logging
 import os 
+import random
 import requests
 import ssl
 import sys
 import websocket
 from datetime import datetime
 
+# set logging level
+logging.basicConfig(filename='websocket_client.log',
+                    level=logging.INFO,
+                    format='%(levelname)s:%(asctime)s:%(message)s')
+
 # set debugging mode for all websocket connection
 websocket.enableTrace(True)
+
 
 
 def _getenvvar(name: str) -> str:
@@ -32,91 +40,71 @@ if __name__ == "__main__":
     api_host = _getenvvar('HOPR_API_HOST')
     api_key  = _getenvvar('HOPR_API_KEY')
 
-    # Get address id of api host and verify that the api host is still available
-    # =========================================================================
-    
+    # get address id of api host and verify that the api host is still available
     address_url = "https://{}:3001/api/v2/account/addresses".format(api_host)
     headers     = {
       'X-Auth-Token': api_key
     }
     address_response = requests.request("GET",
-                                address_url,
-                                headers=headers)
-     
-    if (address_response.status_code == 200): # 200: Address fetched successfully 
-        print("API host is available, ",
-              "Status code:", address_response.status_code,
-              ", address:", json.dumps(address_response.json()['hoprAddress'], indent=4))
+                                        address_url,
+                                        headers=headers)
+    my_pid = None
+    if (address_response.status_code == 200): # 200: Address fetched successfully
+        my_pid = address_response.json()['hoprAddress']
+        logging.info("Host available (PID {})".format(my_pid))
     else:
-        print("API host is NOT available: start a new cluster, ",
-              "Status code:", address_response.status_code)
-        sys.exit(1)   
+        logging.error("Host is NOT available: start a new cluster. Status code: {}".format(address_response.status_code))
+        sys.exit(1)
         
-    # Get information about the api host's peers 
-    # =========================================================================
-    
+    # get information about the api host's peers
     peer_url = "https://{}:3001/api/v2/node/peers".format(api_host)
     headers     = {
       'X-Auth-Token': api_key
     }
     peer_response = requests.request("GET",
-                                peer_url,
-                                headers=headers)
+                                     peer_url,
+                                     headers=headers)
     
-    if (peer_response.status_code == 200): # 200: Peer information fetched successfully 
-        print("Peer information fetched successfully, ",
-              "Status code:", peer_response.status_code)       
-    else:
-        print("Could not fetch peer information, ", 
-              "Status code:", peer_response.status_code)
-        sys.exit(1)   
-    
-    # Create list of peer id's
-    api_host_peers = []
-    for i in range(4):
-        peer = json.dumps(peer_response.json()['connected'][i]['peerId'], indent=4)
-        api_host_peers.append(peer.strip('\"')) # strip: Remove double quotes from string
-    
-    print("Peers:", api_host_peers)
-    
-    # Define message path 
-    # =========================================================================
-    
-    # Select between 1 HOP, 2 HOP, or 3 HOP message 
-    path = [api_host_peers[0]]                                       # 1 HOP Message
-    #path = [api_host_peers[0], api_host_peers[1]]                    # 2 HOP Message
-    #path = [api_host_peers[0], api_host_peers[1], api_host_peers[2]] # 3 HOP Message
-    
-    # Send a meesage to a peer  
-    # =========================================================================
-    
-    send_url  = "https://{}:3001/api/v2/messages/".format(api_host)
-    recv_peer = address_response.json()['hoprAddress'] # sets api host as recipient 
-    #recv_peer = api_host_peers[1] # set other peer as the recipient 
-    print("Message Recipient:", recv_peer)
+    if (peer_response.status_code != 200):
+        logging.error("Could not fetch peer information. Status code: {}".format(peer_response.status_code))
+        sys.exit(1)
 
-    headers = {
+    # double check that the number of connected peers is not greater than the announced ones
+    connected_peers = peer_response.json()['connected']
+    announced_peers = peer_response.json()['announced']
+    assert(len(connected_peers) <= len(announced_peers))
+
+    # create list of peer id's
+    api_host_peers = []
+    for i in range(len(connected_peers)):
+        peer = json.dumps(peer_response.json()['connected'][i]['peerId'], indent=4)
+        api_host_peers.append(peer.strip('"'))
+
+    # my PID should never appear as one of my peers
+    assert(my_pid not in api_host_peers)
+
+    # send a meesage to ourselves through a random peer
+    path     = [random.choice(api_host_peers)]
+    send_url = "https://{}:3001/api/v2/messages/".format(api_host)
+    headers  = {
       'X-Auth-Token': api_key,
       'Content-Type': 'application/json'
     }
+    logging.info("Sending message to self through: {}".format(path))
 
     payload = json.dumps({
       "body": str(datetime.now()), # send current time stamp as message content 
-      "recipient": recv_peer,
+      "recipient": my_pid,
       "path": path
     })
 
     send_response = requests.request("POST",
-                                send_url,
-                                headers=headers,
-                                data=payload)
+                                     send_url,
+                                     headers=headers,
+                                     data=payload)
     
-    if (send_response.status_code == 202): # 202: message sent successfully 
-        print("Message sent successfully, ",
-              "Status code:", send_response.status_code)       
-    else:
-        print("Could not send message, ", 
-              "Status code:", send_response.status_code)
+    if (send_response.status_code != 202): # 202: message sent successfully
+        logging.error("Could not send message. Status code: {}".format(send_response.status_code))
         sys.exit(1)
     
     sys.exit(1)
