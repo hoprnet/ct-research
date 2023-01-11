8ff09122592e4087a9862b40a30d9486c8329110
Martin Benedikt Busch <bene.busch1992@gmail.com>
2023-01-11 11:53:58 +0100

    improved viz

--- peer-viz/net_viz.py
+++ peer-viz/net_viz.py
@@ -1,3 +1,7 @@
+# =============================================================================
+# Network visualization 
+# =============================================================================
+
 import random
 import string
 import networkx as nx
@@ -6,56 +10,53 @@ import matplotlib.patches as mpatches
 import matplotlib.cm as cm
 import matplotlib.colors as colors
 import statistics
+import numpy as np
+import sys
 
-# Generate a random string to use as the name of the dictionary
-name = ''.join(random.choices(string.ascii_letters, k=4))
+# generate any number of random dictionaries 
+# Note: These dictionaries will be importet (only for illustration)
+num_dicts = 5
+num_keys = 4
+num_latency = 5
+for i in range(1,num_dicts+1):
+    locals()["dict_"+str(i)] = {''.join(random.sample(string.ascii_letters, 4)):
+                               [random.randint(100, 1000) for _ in range(num_latency)] for _ in range(num_keys)}
 
-# Create an empty dictionary with the name as the key
-d = {name: {}}
+# generate list of random names (i.e. peer_id of the api_hosts)       
+# Note: The list of names will be importet (only for illustration)
+names = [''.join(random.sample(string.ascii_letters, 4)) for _ in range(num_dicts)]
 
-# Generate 4 random strings of length 4
-keys = [''.join(random.choices(string.ascii_letters, k=4)) for _ in range(4)]
+# example for main api_host (i.e. colored differently than the others) 
+main_api_host = names[0]
 
-# Add a list of 5 random numbers as the value for each key in the inner dictionary
-for key in keys:
-    d[name][key] = [random.randint(1, 10) for _ in range(5)]
+# include names as the name key (i.e. the peer_id's of the api_hosts)
+for i in range(1,num_dicts+1):
+    locals()["dict_"+str(i)] = { names[i-1]: locals()["dict_"+str(i)] }        
+        
+# create a list of dictionaries 
+dicts = []
+for i in range(1, num_dicts+1):
+    dicts.append(eval("dict_"+str(i)))
+    
+# Combine a list of dictionaries into a single dictionary 
+combined = {k: v for d in dicts for k, v in d.items()}
 
-print(d)
-
-# The dictionary of dictionaries representing the graph
-graph = {
-    'Network 1': {
-        'A': {},
-        'B': {},
-        'C': {},
-        'D': {},
-        'E': {}
-    }
-}
-
-# Add all combinations of edges to the inner dictionary
-for node1, connections1 in graph['Network 1'].items():
-    for node2, connections2 in graph['Network 1'].items():
-        # Skip self-edges
-        if node1 == node2:
-            continue
-        # Add the edge to the inner dictionary
-        graph['Network 1'][node1][node2] = [random.randint(1, 10) for _ in range(5)]
-
+# Create a graph dictionary 
+graph = {'Network': combined}
 print(graph)
 
 # Create an empty list to store the edge tuples
 edges = []
 
 # Extract the edge tuples and the edge attributes from the graph dictionary
-for node, connections in graph['Network 1'].items():
+for node, connections in graph['Network'].items():
     for connection, data in connections.items():
         # Compute the median value of the data
         median = statistics.median(data)
         # Add the median value to the edge attributes
         edges.append((node, connection, {'latency': data, 'median': median}))
                 
-# Create the graph using the from_dict_of_dicts method
+# Create the directed graph 
 G = nx.DiGraph()
 G.add_edges_from(edges)  
       
@@ -64,7 +65,7 @@ edge_colors = []
 for u, v, data in G.edges.data():
     median = data['median']
     # Use a color map to map the median value to a color
-    color = plt.cm.RdYlGn(median / 10.0)
+    color = plt.cm.RdYlGn(median / 1000.0) 
     edge_colors.append(color)
 nx.set_edge_attributes(G, edge_colors, 'color')
 
@@ -75,39 +76,66 @@ degree = dict(G.degree(G.nodes()))
 nx.set_node_attributes(G, degree, 'degree')
 
 # Print the attributes of the nodes in the graph
-for node, data in G.nodes.data():
-    print(f'Node {node} has attributes {data}')
+# for node, data in G.nodes.data():
+#    print(f'Node {node} has attributes {data}')
 
 # Print the attributes of the edges in the graph
-nx.get_edge_attributes(G, 'median')
+# nx.get_edge_attributes(G, 'median')
 
 # plot and save peer network 
 plt.figure(figsize=(10, 10), dpi=100, frameon=True)
 
-pos = nx.circular_layout(G)
-color_map = ['red' if node == 'A' else 'yellow' for node in G]
+pos = nx.spring_layout(G)
+#pos = nx.circular_layout(G)
+color_map = ['red' if node == names[1] else 'yellow' for node in G]
 
 nx.draw_networkx_nodes(G, pos, node_color=color_map, node_size=200, edgecolors='black')
-nx.draw_networkx_edges(G, pos, width=0.8, connectionstyle="arc3,rad=0.1", edge_color=edge_colors)
+nx.draw_networkx_edges(G, pos, width=1, connectionstyle="arc3,rad=0.1", edge_color=edge_colors)
 nx.draw_networkx_labels(G, pos, labels=degree, font_size=10)
 
 # Create a ScalarMappable to map the edge colors to a col
-sm = cm.ScalarMappable(cmap=plt.cm.RdYlGn, norm=colors.Normalize(vmin=1, vmax=10))
+sm = cm.ScalarMappable(cmap=plt.cm.RdYlGn, norm=colors.Normalize(vmin=100, vmax=1000))
 sm.set_array([])
 
 # Add a colorbar to the plot
-cbar = plt.colorbar(sm, ticks=range(1, 11))
-cbar.set_label('Latency', rotation=270, labelpad=20)
+cbar = plt.colorbar(sm, ticks= np.linspace(100,1000,11)) 
+cbar.set_label('Median Latency', rotation=270, labelpad=10)
 
-label_patch = mpatches.Patch(color='none', label='Labels represent node degrees')
+label_patch = mpatches.Patch(color='none', label='Node labels represent node degrees')
 plt.legend(handles=[label_patch], loc='upper left', frameon=False)
 
 plt.axis('off')
 plt.tight_layout()
 plt.savefig("net_viz.pdf", pad_inches=0)
+plt.savefig("net_viz.png", pad_inches=0)
 plt.show()
 
+sys.exit(1)
 
+# =============================================================================
+# Alternative plottting a complete graph 
+# =============================================================================
 
+# The dictionary of dictionaries representing the graph
+graph = {
+    'Network': {
+        'A': {},
+        'B': {},
+        'C': {},
+        'D': {},
+        'E': {}
+    }
+}
+
+# Add all combinations of edges to the inner dictionary
+for node1, connections1 in graph['Network'].items():
+    for node2, connections2 in graph['Network'].items():
+        # Skip self-edges
+        if node1 == node2:
+            continue
+        # Add the edge attributes to the inner dictionary
+        graph['Network'][node1][node2] = [random.randint(100, 1000) for _ in range(5)]
+
+print(graph)
 
 
