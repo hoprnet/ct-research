2365a66f5eba79e16f77d2efe9b34af0e1666fbb
Martin Benedikt Busch <43137759+MartinBenediktBusch@users.noreply.github.com>
2023-02-01 16:59:52 +0100

    Delete run.sh

--- ct-dApp/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-export HOPR_NODE_1='192.168.1.135'
-export HOPR_NODE_1_HTTP_URL="http://${HOPR_NODE_1}:3001"
-export HOPR_NODE_1_WS_URL="ws://${HOPR_NODE_1}:3000"
-export HOPR_NODE_1_API_KEY='!5qxc9Lp1BE7IFQ-nrtttU'
-
-python ct.py
