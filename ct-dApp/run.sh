695f3ceaf4ba9bdf1b2bffddb3e99a7129438dba
MartinBenediktBusch <bene.busch1992@gmail.com>
2023-02-27 14:57:43 +0100

    changed write access for files

--- ct-dApp/run.sh
+++ ct-dApp/run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-export HOPR_NODE_1='zero_elbe_white_deimos.playground.hoprnet.org'
+export HOPR_NODE_1='192.168.1.135'
 export HOPR_NODE_1_HTTP_URL="http://${HOPR_NODE_1}:3001"
 export HOPR_NODE_1_WS_URL="ws://${HOPR_NODE_1}:3000"
-export HOPR_NODE_1_API_KEY='3#BA8d4b8e7d720D40e9daf2'
+export HOPR_NODE_1_API_KEY='!5qxc9Lp1BE7IFQ-nrtttU'
 
-python ct.py
+python3 ct.py
