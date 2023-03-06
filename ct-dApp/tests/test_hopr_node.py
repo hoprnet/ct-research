6b1c3c61494bebc31fa3b457d06f2705149cd564
Lucas Benedicic <lucas.benedicic@hoprnet.org>
2023-03-06 09:31:02 +0100

    Test captures set-change error.-

--- ct-dApp/tests/test_hopr_node.py
+++ ct-dApp/tests/test_hopr_node.py
@@ -1,5 +1,4 @@
-import pytest
-
+import asyncio
 from hopr_node import HoprNode
 
 key = 'something'
@@ -12,23 +11,41 @@ def test_req():
     assert request.target_url == "{}/api/v2{}".format(url, end_point)
 
 
-class MockHoprNode(HoprNode):
-    def __init__(self, url: str, key: str):
-        """
-        Patched constructor: always started
-        """
-        super().__init__(url, key)
-        self.started = True
-
-    def _req(*args, **kwargs) -> dict[str, str]:
-        """
-        Patch HoprNode._req to return a valid JSON object.
-        """
-        return {'a': 'b'}
-
-
 def test_adding_peers_while_pinging() -> None:
     """
     Changing the 'peers' set while pinging should not break.
     """
+    class MockHoprNode(HoprNode):
+        def __init__(self, url: str, key: str):
+            """
+            Patched constructor: connected and started
+            """
+            super().__init__(url, key)
+            self.started = True
+
+        def _req(*args, **kwargs) -> dict[str, str]:
+            """
+            Patch HoprNode._req to return a valid JSON object.
+            """
+            return {'a': 'b'}
+
+        async def connect(self):
+            self.peer_id = "testing_peer_id"
+            while self.started:
+                await asyncio.sleep(45)
+
+        async def gather_peers(self):
+            """
+            Patched to discover one additional peer every couple of seconds
+            """
+            while self.started:
+                await asyncio.sleep(1)
+                peer = 'peer_{}'.format(len(self.peers))
+                self.peers.add(peer)
+
     node = MockHoprNode("some_host", "some_key")
+    loop = asyncio.new_event_loop()
+
+    loop.call_later(10, lambda: node.stop())
+    loop.run_until_complete(node.start())
+    loop.close()
