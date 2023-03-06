f8f979e141de9066c70e2c53f3859275741e9a87
MartinBenediktBusch <bene.busch1992@gmail.com>
2023-03-06 16:41:51 +0100

    review + suggested solution

--- ct-dApp/tests/test_hopr_node.py
+++ ct-dApp/tests/test_hopr_node.py
@@ -1,14 +1,21 @@
 import asyncio
 from hopr_node import HoprNode
 
-key = 'something'
-url = "some_url"
-end_point = "/account/addresses"
+def test_url_format():
+    """
+    Test whether the target url is returned correctly.
+    """
+    class MockHoprNode(HoprNode):
+        def __init__(self, url: str, key: str):
+            """
+            Patched url and api_key
+            """
+            super().__init__(url, key)
 
-def test_req():
-    node = HoprNode(url, key)  # Create an instance of the HoprNode class
-    request = node._req(end_point=end_point, method='GET', payload=None) 
-    assert request.target_url == "{}/api/v2{}".format(url, end_point)
+    node = MockHoprNode("some_url", "some_key")
+    end_point = "/node/some_endpoint"
+    target_url = "{}/api/v2{}".format(node.url, end_point)
+    assert target_url == "some_url/api/v2/node/some_endpoint"
 
 
 def test_adding_peers_while_pinging() -> None:
@@ -43,7 +50,7 @@ def test_adding_peers_while_pinging() -> None:
                 peer = 'peer_{}'.format(len(self.peers))
                 self.peers.add(peer)
 
-    node = MockHoprNode("some_host", "some_key")
+    node = MockHoprNode("some_url", "some_key")
     loop = asyncio.new_event_loop()
 
     loop.call_later(10, lambda: node.stop())
