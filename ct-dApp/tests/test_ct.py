37a776e493a519f646c53274f5aeb2c056f00c71
MartinBenediktBusch <bene.busch1992@gmail.com>
2023-06-05 17:43:20 +0200

    applied black formating

--- ct-dApp/tests/test_ct.py
+++ ct-dApp/tests/test_ct.py
@@ -1,43 +1,46 @@
 import pytest
 import os
-from unittest.mock import MagicMock 
+from unittest.mock import MagicMock
 
 from hopr_node import HoprNode
 from ct import _getenvvar
-from ct import stop  
+from ct import stop
+
 
 def test_getenvvar_load_envar() -> None:
     """
     Test whether the global environment variables are loaded correctly.
     """
-    os.environ['HOPR_NODE_1_HTTP_URL'] = 'http_url'
-    os.environ['HOPR_NODE_1_API_KEY'] = 'api_key'
+    os.environ["HOPR_NODE_1_HTTP_URL"] = "http_url"
+    os.environ["HOPR_NODE_1_API_KEY"] = "api_key"
 
-    envvar_0 = _getenvvar('HOPR_NODE_1_HTTP_URL')
-    envvar_1 = _getenvvar('HOPR_NODE_1_API_KEY')
-    assert envvar_0 == 'http_url'
-    assert envvar_1 == 'api_key' 
+    envvar_0 = _getenvvar("HOPR_NODE_1_HTTP_URL")
+    envvar_1 = _getenvvar("HOPR_NODE_1_API_KEY")
+    assert envvar_0 == "http_url"
+    assert envvar_1 == "api_key"
+
 
 def test_getenvvar_exit() -> None:
     """
     Test whether system exit is called when no environemnt variable is provided.
     """
-    
+
     with pytest.raises(SystemExit) as exc_info:
-        _getenvvar('NO_SUCH_ENV_VAR_EXISTS')
-    
+        _getenvvar("NO_SUCH_ENV_VAR_EXISTS")
+
     assert exc_info.type == SystemExit
     assert exc_info.value.code == 1
- 
+
+
 def test_stop():
     """
-    Test whether the stop function correctly calls the stop method of the node object with the SIGINT signal. 
+    Test whether the stop function correctly calls the stop method of the node object with the SIGINT signal.
     """
     node = HoprNode("some_url", "some_key")
-    
+
     # Create a mock object of the stop method of the HoprNode class to test whether the stop method calls the mock object.
     node.stop = MagicMock()
-    
+
     stop(node, "SIGINT")
 
     node.stop.assert_called_once()
