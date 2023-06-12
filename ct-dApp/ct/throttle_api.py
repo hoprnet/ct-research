6e279fcd8eb2fe82bb9eb5aca5be4661ca376d64
jean.demeusy <dev.jdu@gmail.com>
2023-06-12 11:16:35 +0200

    Added error raising

--- ct-dApp/ct/throttle_api.py
+++ ct-dApp/ct/throttle_api.py
@@ -1,5 +1,5 @@
 from hoprd import wrapper
-
+import httpx
 
 class ThrottledHoprdAPI:
     def __init__(self, url: str, token: str):
@@ -16,48 +16,98 @@ class ThrottledHoprdAPI:
     def token(self) -> str:
         return self._token
     
-    
+    async def _safe_call(self, func, *args, **kwargs):
+        try:
+            response = await func(*args, **kwargs)
+            response.raise_for_status()
+        except httpx.HTTPStatusError as e:
+            if e.response.status_code == 400:
+                raise ValueError(e.response.json()['error'])
+            else:
+                raise e
+        else:
+            return response
+            
     async def withdraw(self, currency, amount, address):
-        return await self.wrapper.withdraw(currency, amount, address)
+        method = self.wrapper.withdraw
+        args = [currency, amount, address]
+
+        return await self._safe_call(method, *args)
     
     async def balance(self):
-        return await self.wrapper.balance()
+         method = self.wrapper.balance
+
+         return await self._safe_call(method)
     
     async def set_alias(self, peer_id, alias):
-        return await self.wrapper.set_alias(peer_id, alias)
+        method = self.wrapper.set_alias
+        args = [peer_id, alias]
+
+        return await self._safe_call(method, *args)
     
     async def get_alias(self, alias):
-        return await self.wrapper.get_alias(alias)
+        method = self.wrapper.get_alias
+        args = [alias]
+
+        return await self._safe_call(method, *args)
     
     async def remove_alias(self, alias):
-        return await self.wrapper.remove_alias(alias)
+        method = self.wrapper.remove_alias
+        args = [alias]
+
+        return await self._safe_call(method, *args)
     
     async def get_settings(self):
-        return await self.wrapper.get_settings()
+        method = self.wrapper.get_settings
+
+        return await self._safe_call(method)
     
     async def get_all_channels(self, include_closed: bool):
-        return await self.wrapper.get_all_channels(include_closed)
+        method = self.wrapper.get_all_channels
+        args = [include_closed]
+
+        return await self._safe_call(method, *args)
     
     async def get_channel_topology(self, full_topology: bool):
-        return await self.wrapper.get_channel_topology(full_topology)
+        method = self.wrapper.get_channel_topology
+        args = [full_topology]
+
+        return await self._safe_call(method, *args)
     
     async def get_tickets_in_channel(self, include_closed: bool):
-        return await self.wrapper.get_tickets_in_channel(include_closed)
+        method = self.wrapper.get_tickets_in_channel
+        args = [include_closed]
+
+        return await self._safe_call(method, *args)
     
     async def redeem_tickets_in_channel(self, peer_id):
-        return await self.wrapper.redeem_tickets_in_channel(peer_id)
+        method = self.wrapper.redeem_tickets_in_channel
+        args = [peer_id]
+
+        return await self._safe_call(method, *args)
     
     async def redeem_tickets(self):
-        return await self.wrapper.redeem_tickets()
+        method = self.wrapper.redeem_tickets
+        return await self._safe_call(method)
     
     async def ping(self, peer_id):
-        return await self.wrapper.ping(peer_id)
+        method = self.wrapper.ping
+        args = [peer_id]
+
+        return await self._safe_call(method, *args)
     
     async def peers(self, **kwargs):
-        return await self.wrapper.peers(**kwargs)
+        method = self.wrapper.peers
+
+        return await self._safe_call(method, **kwargs)
     
     async def get_address(self):
-        return await self.wrapper.get_address()
+        method = self.wrapper.get_address
+
+        return await self._safe_call(method)
     
     async def send_message(self, destination, message, hops):
-        return await self.wrapper.send_message(destination, message, hops)
+        method = self.wrapper.send_message
+        args = [destination, message, hops]
+        
+        return await self._safe_call(method, *args)
