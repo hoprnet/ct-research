86e4b9db3f4babd9ad789c0f713b66bf1d20960d
jean.demeusy <dev.jdu@gmail.com>
2023-06-13 18:40:49 +0200

    class renaming

--- ct-dApp/ct/__init__.py
+++ ct-dApp/ct/__init__.py
@@ -1,6 +1,6 @@
 from .exit_codes import ExitCode
 from .hopr_node import HOPRNode
-from .throttle_api import ThrottledHoprdAPI
+from .throttle_api import HoprdAPIHelper
 from .utils import _getlogger, _getenvvar, stop
 
-__all__ = ["HOPRNode", "ExitCode", "ThrottledHoprdAPI", "_getlogger", "_getenvvar", "stop"]
+__all__ = ["HOPRNode", "ExitCode", "HoprdAPIHelper", "_getlogger", "_getenvvar", "stop"]
