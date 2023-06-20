826776d2424052c013fe012574511e5b30936039
Jean Demeusy <61140535+jeandemeusy@users.noreply.github.com>
2023-06-20 14:53:10 +0200

    added transmission without db

--- ct-dApp/netwatcher/__main__.py
+++ ct-dApp/netwatcher/__main__.py
@@ -1,17 +1,40 @@
+import asyncio
+from signal import SIGINT, SIGTERM
+
 from ct.exit_codes import ExitCode
-from ct.utils import _getenvvar
+from ct.utils import _getenvvar, _getlogger, stop
 
 from .netwatcher import NetWatcher
 
 
 def main():
+    log = _getlogger()
+    exit_code = ExitCode.OK
+    
     try:
         API_host = _getenvvar("HOPR_NODE_1_HTTP_URL")
         API_key = _getenvvar("HOPR_NODE_1_API_KEY")
     except ValueError:
         exit(ExitCode.ERROR_BAD_ARGUMENTS)
         
-    NetWatcher(API_host, API_key)
+    nw = NetWatcher(API_host, API_key)
+
+    loop = asyncio.new_event_loop()
+    loop.add_signal_handler(SIGINT, stop, nw, SIGINT)
+    loop.add_signal_handler(SIGTERM, stop, nw, SIGTERM)
+
+    # start the node and run the event loop until the node stops
+    try:
+        loop.run_until_complete(nw.start())
+
+    except Exception as e:
+        log.error("Uncaught exception ocurred", str(e))
+        exit_code = ExitCode.ERROR_UNCAUGHT_EXCEPTION
+
+    finally:
+        nw.stop()
+        loop.close()
+        exit(exit_code)
 
 
 if __name__ == "__main__":
--- ct-dApp/netwatcher/netwatcher.py
+++ ct-dApp/netwatcher/netwatcher.py
@@ -1,4 +1,48 @@
+import functools
+import logging
+import aiohttp
+
+import asyncio
 from ct.hopr_node import HOPRNode
+
+log = logging.getLogger(__name__)
+
+
+def formalin(message: str = None, sleep: int = None):
+    """
+    Decorator to log the start of a function, make it run until stopped, and delay the
+    next iteration
+    :param message: the message to log when the function starts
+    :param sleep: the duration to sleep after an interation
+    """
+    def decorator(func):
+        @functools.wraps(func)
+        async def wrapper(self, *args, **kwargs):
+            if message is not None:
+                log.info(message)
+
+            while self.started:
+                await func(self, *args, **kwargs)
+
+                if sleep is not None:
+                    await asyncio.sleep(sleep)
+
+        return wrapper
+    return decorator
+
+def connectguard(func):
+    """
+    Decorator to check if the node is connected before running anything
+    """
+    @functools.wraps(func)
+    async def wrapper(self, *args, **kwargs):
+        if not self.connected:
+            log.warning("Node not connected, skipping")
+            return
+
+        await func(self, *args, **kwargs)
+
+    return wrapper
 
 class NetWatcher(HOPRNode):
     """ Class description."""
@@ -8,5 +52,41 @@ class NetWatcher(HOPRNode):
         """
         super().__init__(url, key, 10, '.')
     
+
+    @formalin(sleep=20)
+    @connectguard
+    async def transmit_peers(self, mocked: str = True):
+        """
+        Sends the detected peers to the Aggregator
+        """
+        log.info("Transmitting peers to Aggregator")
+        url = "http://localhost:3000/ct/fin_dist/aggregator/send_list"
+
+        if not mocked:
+            async with aiohttp.ClientSession() as session:
+                data = {"terms": 1, "captcha": 1}
+                async with session.post(url, data) as response:
+                    data = await response.text()
+                    print(data)
+
+        sent_list = [p[-5:] for p in self.peers]
+        log.info(f"Transmisted peers: {', '.join(sent_list)}")
+        self.peers.clear()
+        
+
+    async def start(self):
+        """
+        Starts the tasks of this node
+        """
+        log.info("Starting node")
+        if self.tasks:
+            return
+    
+        self.started = True
+        self.tasks.add(asyncio.create_task(self.connect(address="hopr")))
+        self.tasks.add(asyncio.create_task(self.gather_peers()))
+        self.tasks.add(asyncio.create_task(self.transmit_peers()))
+        await asyncio.gather(*self.tasks)
+
     def __str__(self):
         return
