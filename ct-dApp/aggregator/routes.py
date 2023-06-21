154050e089bd2865d654b95446633ad20e46335a
Jean Demeusy <jdemeusy@me.com>
2023-06-21 17:24:20 +0200

    POST route now returns status code

--- ct-dApp/aggregator/routes.py
+++ ct-dApp/aggregator/routes.py
@@ -7,17 +7,17 @@ def setup_routes(app):
     @app.route("/aggregator/list", methods=["POST"])
     async def post_list(request):
         if "id" not in request.json:
-            return sanic_text("Bad id")
+            return sanic_text("`id` key not in body", status=500)
         if "list" not in request.json:
-            return sanic_text("Bad content")
+            return sanic_text("`list` key not in body", status=500)
         
         pod_id = request.json["id"]
         data_list = request.json["list"]
         
         agg.add({pod_id: data_list})
 
-        return sanic_text(f"Received information for {len(data_list)} peers")
+        return sanic_text(f"Received list of length {len(data_list)}", status=200)
     
     @app.route("/aggregator/list", methods=["GET"])
     async def get_list(request):
-        return sanic_text(f"{agg.get()}")
+        return sanic_text(f"{agg.get()}", status=200)
