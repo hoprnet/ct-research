0b4367c5041727f6600135edfa3dde37401ad416
Jean Demeusy <jdemeusy@me.com>
2023-06-21 17:23:20 +0200

    added post url as env variable

--- ct-dApp/run.sh
+++ ct-dApp/run.sh
@@ -4,6 +4,7 @@ export HOPR_NODE_1='127.0.0.1'
 export HOPR_NODE_1_HTTP_URL="http://${HOPR_NODE_1}:13305"
 export HOPR_NODE_1_WS_URL="ws://${HOPR_NODE_1}:13305"
 export HOPR_NODE_1_API_KEY='%th1s-IS-a-S3CR3T-ap1-PUSHING-b1ts-TO-you%'
+export AGG_HTTP_POST_URL='http://localhost:8080/aggregator/list'
 
 while getopts e:: flag
 do
@@ -12,6 +13,7 @@ do
     esac
 done
 
+clear
 if [ "$element" = "ct" ]; then
     echo "Running ct"
     python -m ct --plotf plots --latcount 100
