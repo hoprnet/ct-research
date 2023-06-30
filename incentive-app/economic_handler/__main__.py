6745d77a9f421feead6987f88afc43fbac463c56
Jean Demeusy <jdemeusy@me.com>
2023-06-30 16:32:32 +0200

    tried to make economic handler run continuously

--- incentive-app/economic_handler/__main__.py
+++ incentive-app/economic_handler/__main__.py
@@ -1,56 +1,51 @@
 import asyncio
-import logging
-import traceback
 from signal import SIGINT, SIGTERM, Signals
-import click
 
 from tools.exit_codes import ExitCode
 from tools.utils import _getenvvar
 from .economic_handler import EconomicHandler
+
+
+def stop(instance: EconomicHandler, signal: Signals):
+    """Stop the economic handler instance when a signal is received"""
+    print(f">>> Caught signal {signal.name} <<<")
+    instance.stop()
+
 
 def main():
     """main"""
     try:
         API_host = _getenvvar("HOPR_NODE_1_HTTP_URL")
         API_key = _getenvvar("HOPR_NODE_1_API_KEY")
-        RPCH_nodes = _getenvvar("RPCH_NODES_API_ENDPOINT")
+        _getenvvar("RPCH_NODES_API_ENDPOINT")
     except ValueError:
         exit(ExitCode.ERROR_BAD_ARGUMENTS)
 
     economic_handler = EconomicHandler(API_host, API_key)
 
-    tasks = [
-        economic_handler.channel_topology(),
-        economic_handler.read_parameters_and_equations(),
-        economic_handler.blacklist_rpch_nodes(api_endpoint=RPCH_nodes)
-    ]
+    # tasks = [
+    #     economic_handler.channel_topology(),
+    #     economic_handler.read_parameters_and_equations(),
+    #     economic_handler.blacklist_rpch_nodes(api_endpoint=RPCH_nodes),
+    # ]
 
-    loop = asyncio.get_event_loop()
-    result = loop.run_until_complete(asyncio.gather(*tasks))
-    channel_topology, parameters_equations_budget, blacklist = result
+    loop = asyncio.new_event_loop()
+    loop.add_signal_handler(SIGINT, stop, economic_handler, SIGINT)
+    loop.add_signal_handler(SIGTERM, stop, economic_handler, SIGTERM)
 
-    # helper functions that allow to test the code (subject to removal)
-    result_1 = economic_handler.replace_keys_in_mock_data(channel_topology)
-    result_2 = economic_handler.replace_keys_in_mock_data_subgraph(channel_topology)
+    # start the node and run the event loop until the node stops
+    try:
+        loop.run_until_complete(economic_handler.start())
 
-    # merge channel topology with metrics from the database and subgraph data
-    result_3 = economic_handler.merge_topology_metricdb_subgraph(channel_topology,
-                                                                result_1, result_2
-                                                                )
+    except Exception as e:
+        print("Uncaught exception ocurred", str(e))
+        exit_code = ExitCode.ERROR_UNCAUGHT_EXCEPTION
 
-    # computation of cover traffic probability
-    result_4 = economic_handler.compute_ct_prob(parameters_equations_budget[0],
-                                                parameters_equations_budget[1],
-                                                result_3)
+    finally:
+        economic_handler.stop()
+        loop.close()
+        exit(exit_code)
 
-    # calculate expected rewards and output it as a csv file
-    result_5 = economic_handler.compute_expected_reward_savecsv(result_4,
-                                                                parameters_equations_budget[2]
-                                                                )
-
-    print(blacklist) # RPCh nodes blacklist (not yet included: would need to mock it)
-    print(result_5)
 
 if __name__ == "__main__":
     main()
-
