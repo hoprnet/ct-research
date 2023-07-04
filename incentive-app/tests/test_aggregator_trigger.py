2a077ff0232dd6909c79e7dd13c005031c189615
Jean Demeusy <jdemeusy@me.com>
2023-07-04 13:34:07 +0200

    removed unncessary test

--- incentive-app/tests/test_aggregator_trigger.py
+++ incentive-app/tests/test_aggregator_trigger.py
@@ -9,11 +9,3 @@ def test_url_construction():
     instance = AggregatorTrigger("gcp.host.com", 5000, "/agg/send_list_to_db")
 
     assert validators.url(instance.endpoint_url)
-
-
-def test_good_request():
-    assert False
-
-
-def test_bad_request():
-    assert False
