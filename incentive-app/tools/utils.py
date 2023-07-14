2032fbf3254a8ab6b9c39f3a602cdbcd6897e75f
Jean Demeusy <jdemeusy@me.com>
2023-07-14 09:30:13 +0200

    add a way to cast envvars on method call

--- incentive-app/tools/utils.py
+++ incentive-app/tools/utils.py
@@ -7,32 +7,41 @@ import click
 
 from .hopr_node import HOPRNode
 
+
 def _getlogger() -> logging.Logger:
     """
     Generate a logger instance based on folder and name.
     :returns: a tuple with the logger instance and the name of the log file
     """
-        
+
     # configure and get logger handler
-    logging.basicConfig(level=logging.INFO, format="%(levelname)s:%(asctime)s:%(message)s")
-    
+    logging.basicConfig(
+        level=logging.INFO, format="%(levelname)s:%(asctime)s:%(message)s"
+    )
+
     logger = logging.getLogger(__name__)
-    logging.getLogger("httpx").setLevel(logging.WARNING) # logs were flooded by httpx 
+    logging.getLogger("httpx").setLevel(logging.WARNING)  # logs were flooded by httpx
 
     return logger
 
 
-def _getenvvar(name: str) -> str:
+def envvar(name: str, type: type = None) -> str:
     """
-    Gets the string contained in environment variable 'name'.
+    Gets the string contained in environment variable 'name' and casts it to 'type'.
     :param name: name of the environment variable
+    :param type: type to cast the variable to
     :returns: the string contained in the environment variable
     :raises ValueError: if the environment variable is not found
     """
     if os.getenv(name) is None:
         raise ValueError(f"Environment variable [{name}] not found")
 
-    return os.getenv(name)
+    value = os.getenv(name)
+
+    if type:
+        return type(value)
+
+    return value
 
 
 def stop(node: HOPRNode, caught_signal: Signals):
