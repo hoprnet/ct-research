017ae2fde937942e89dc03a95c2810cb3bd91b5a
Jean Demeusy <jdemeusy@me.com>
2023-08-02 15:52:40 +0200

    use the new logger class

--- ct-app/tools/utils.py
+++ ct-app/tools/utils.py
@@ -1,25 +1,30 @@
 import logging
 import os
 import logging.config
+import sys
 import jsonschema
 import json
+from .logger import ColoredLogger
 
 
-def _getlogger() -> logging.Logger:
+def getlogger() -> logging.Logger:
     """
     Generate a logger instance based on folder and name.
     :returns: a tuple with the logger instance and the name of the log file
     """
-
     # configure and get logger handler
-    logging.basicConfig(
-        level=logging.INFO, format="%(levelname)s:%(asctime)s:%(message)s"
-    )
+    module = running_module(uppercase=True)
+    if not module:
+        module = "main"
 
-    logger = logging.getLogger(__name__)
-    logging.getLogger("httpx").setLevel(logging.WARNING)  # logs were flooded by httpx
+    # checks if a logger already exists with the given module name
+    if logging.getLoggerClass() != ColoredLogger:
+        logging.setLoggerClass(ColoredLogger)
+        logging.getLogger("httpx").setLevel(
+            logging.WARNING
+        )  # logs were flooded by httpx
 
-    return logger
+    return logging.getLogger(module)
 
 
 def envvar(name: str, type: type = None) -> str:
@@ -48,7 +53,7 @@ def read_json_file(path, schema):
     ;param: schema: The validation schema
     :returns: (dict): The contents of the JSON file.
     """
-    log = _getlogger()
+    log = getlogger()
     try:
         with open(path, "r") as file:
             contents = json.load(file)
@@ -68,3 +73,17 @@ def read_json_file(path, schema):
         return {}
 
     return contents
+
+
+def running_module(uppercase: bool = False):
+    argv = sys.argv[0]
+
+    if not argv.endswith("__main__.py"):
+        return None
+
+    module = argv.split("/")[-2]
+
+    if uppercase:
+        module = module.upper()
+
+    return module
