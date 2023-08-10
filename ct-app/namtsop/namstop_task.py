76d731037009782feac47de9c901daa7aa2db01e
Jean Demeusy <jdemeusy@me.com>
2023-08-10 12:17:32 +0200

    added base db communication logic

--- ct-app/namtsop/namstop_task.py
+++ ct-app/namtsop/namstop_task.py
@@ -1,7 +1,16 @@
-import asyncio
-
 from celery import Celery
 from tools import envvar, getlogger
+
+from tools.db_connection import DatabaseConnection
+
+_db_columns = [
+    ("id", "SERIAL PRIMARY KEY"),
+    ("peer_id", "VARCHAR(255) NOT NULL"),
+    ("node_address", "VARCHAR(255) NOT NULL"),
+    ("count", "INTEGER NOT NULL"),
+    ("timestamp", "TIMESTAMP NOT NULL DEFAULT NOW()"),
+]
+
 
 log = getlogger()
 
@@ -12,11 +21,28 @@ app = Celery(
 )
 
 
-# the name of the task is the name of the "<task_name>.<node_address>"
-@app.task(name=f"{envvar('TASK_NAME')}")
-def foo_task():
-    return asyncio.run(foo_task())
+# the name of the task is the name of the "<task_name>"
+@app.task(name="foo_task")
+def foo_task(peer_id: str, node_address: str, count: int):
+    """
+    Celery task to ..."""
 
+    with DatabaseConnection(
+        envvar("DB_NAME"),
+        envvar("DB_HOST"),
+        envvar("DB_USER"),
+        envvar("DB_PASSWORD"),
+        envvar("DB_PORT", int),
+    ) as db:
+        try:
+            db.create_table("rewardTable", _db_columns)
+        except ValueError as e:
+            log.warning(f"Error creating table: {e}")
 
-async def async_foo():
-    pass
+        if not db.table_exists_guard("raw_data_table"):
+            log.error("Table not available, not sending to DB")
+            return
+
+        db.insert(
+            "rewardTable", peer_id=peer_id, node_address=node_address, count=count
+        )
