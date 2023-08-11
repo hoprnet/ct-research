6021d047b1fcc1e487cc3191cfcc3ab92dec35ce
Jean Demeusy <jdemeusy@me.com>
2023-08-11 15:45:56 +0200

    refactor for readability + timeout implemented

--- ct-app/postman/postman_tasks.py
+++ ct-app/postman/postman_tasks.py
@@ -1,4 +1,5 @@
 import asyncio
+import time
 
 from celery import Celery
 from tools import HoprdAPIHelper, envvar, getlogger
@@ -8,10 +9,16 @@ log = getlogger()
 
 
 class TaskStatus(Enum):
+    """
+    Enum to represent the status of a task. This status is also used when creating a
+    task in the outgoing queue.
+    """
+
+    DEFAULT = "DEFAULT"
     SUCCESS = "SUCCESS"
-    FAILED = "FAILED"
-    RETRYING = "RETRYING"
+    RETRIED = "RETRIED"
     SPLITTED = "SPLITTED"
+    TIMEOUT = "TIMEOUT"
 
 
 app = Celery(
@@ -21,35 +28,50 @@ app = Celery(
 )
 
 
-# the name of the task is the name of the "<task_name>.<node_address>"
-@app.task(name=f"{envvar('TASK_NAME')}.{envvar('NODE_ADDRESS')}")
-def send_1_hop_message(
-    peer: str, count: int, node_list: list[str], node_index: int
-) -> TaskStatus:
+def loop_through_nodes(node_list: list[str], node_index: int) -> tuple[str, int]:
     """
-    Celery task to send `count`1-hop messages to a peer.
-    This method is the entry point for the celery worker. As the task that is executed
-    relies on asyncio, we need to run it in a decated event loop. The only call this
-    method does is to run the async method `async_send_1_hop_message`.
-    :param peer: Peer ID to send messages to.
-    :param count: Number of messages to send.
-    :param node_list: List of nodes connected to this peer, they can serve as backups.
-    :param node_index: Index of the node in the list of nodes.
+    Get the next node address in the list of nodes. If the index is out of bounds, it
+    will be reset to 0.
+    :param node_list: List of nodes to loop through.
+    :param node_index: Index of the current node.
+    :return: Tuple containing the next node address and the next node index.
     """
-    return asyncio.run(async_send_1_hop_message(peer, count, node_list, node_index))
-
-
-def get_next_node_address(node_list: list[str], node_index: int) -> tuple[str, int]:
-    if node_index == len(node_list) - 1:
-        return None, None
-
-    node_index += 1
+    node_index = (node_index + 1) % len(node_list)
 
     return node_list[node_index], node_index
 
 
+# the name of the task is the name of the "<task_name>.<node_address>"
+@app.task(name=f"{envvar('TASK_NAME')}.{envvar('NODE_ADDRESS')}")
+def send_1_hop_message(
+    peer: str,
+    message_count: int,
+    node_list: list[str],
+    node_index: int,
+    timestamp: float = time.time(),
+) -> TaskStatus:
+    """
+    Celery task to send `message_count`1-hop messages to a peer.
+    This method is the entry point for the celery worker. As the task that is executed
+    relies on asyncio, we need to run it in a decated event loop. The only call this
+    method does is to run the async method `async_send_1_hop_message`.
+    :param peer: Peer ID to send messages to.
+    :param message_count: Number of messages to send.
+    :param node_list: List of nodes connected to this peer, they can serve as backups.
+    :param node_index: Index of the node in the list of nodes.
+    :param timestamp: Timestamp at first iteration. For timeout purposes.
+    """
+    return asyncio.run(
+        async_send_1_hop_message(peer, message_count, node_list, node_index, timestamp)
+    )
+
+
 async def async_send_1_hop_message(
-    peer_id: str, count: int, node_list: list[str], node_index: int
+    peer_id: str,
+    message_count: int,
+    node_list: list[str],
+    node_index: int,
+    timestamp: float,
 ) -> TaskStatus:
     """
     Celery task to send `count`1-hop messages to a peer in an async manner.
@@ -59,8 +81,17 @@ async def async_send_1_hop_message(
     :param node_index: Index of the node in the list of nodes.
     """
 
+    # at the first iteration, the timestamp is set to the current time. At each task
+    # transfer, the method will check if the timestamp is recent enough to consider
+    # trying to send a message
+    if time.time() - timestamp > 60 * 60 * 2:  # timestamp is older than 2 hours
+        log.error("Trying to send a message for more than 2 hours, stopping")
+        return TaskStatus.TIMEOUT
+
     api_host = envvar("API_HOST")
     api_token = envvar("API_TOKEN")
+    status = TaskStatus.DEFAULT
+    sent_messages = 0
 
     api = HoprdAPIHelper(api_host, api_token)
 
@@ -76,60 +107,43 @@ async def async_send_1_hop_message(
 
     # if the node is not reachable, the task is tranfered to the next node in the list
     if address is None:
-        log.error("Could not get connect to node. Trying with a backup node")
+        log.error("Could not get connect to node. Transfering task to the next node.")
+        status = TaskStatus.RETRIED
 
-        node_address, node_index = get_next_node_address(node_list, node_index)
+    while status == TaskStatus.DEFAULT:
+        # node is reachable, messages can be sent
+        success_sending = await api.send_message(address, "foo", [peer_id])
 
-        # if there are no more nodes in the list, the task is stopped
-        if not node_address:
-            log.info("This is the last node in the list, stopping")
-            return TaskStatus.FAILED
+        if not success_sending:
+            log.error(
+                "Could not send message. Transfering remaining ones to the next node."
+            )
+            status = TaskStatus.SPLITTED
+        else:
+            sent_messages += 1
 
-        log.info(f"Redirecting task to {node_address} (#{node_index} - {api_host})")
+        if sent_messages == message_count:
+            status = TaskStatus.SUCCESS
+
+    log.info(f"{sent_messages} messages sent to `{peer_id}` via {address} ({api_host})")
+
+    if status != TaskStatus.SUCCESS:
+        node_address, node_index = loop_through_nodes(node_list, node_index)
+        log.info(
+            f"Creating task for {node_address}: {sent_messages}/{message_count} "
+            + "messages already sent"
+        )
 
         app.send_task(
             f"{envvar('TASK_NAME')}.{node_address}",
-            args=(peer_id, count, node_list, node_index),
+            args=(
+                peer_id,
+                message_count - sent_messages,
+                node_list,
+                node_index,
+                timestamp,
+            ),
             queue=node_address,
         )
 
-        return TaskStatus.RETRYING
-
-    log.info(
-        f"Sending {count} messages to `{peer_id}` "
-        + f"via {node_list[node_index]}(#{node_index} - {api_host})"
-    )
-
-    # node is reachable, messages can be sent
-    for idx in range(count):
-        state = await api.send_message(address, "foo", [peer_id])
-
-        if not state:
-            log.error("Could not send message")
-            break
-    else:
-        log.info(
-            f"{count} messages sent to `{peer_id}` "
-            + f"via {node_list[node_index]}(#{node_index} - {api_host})"
-        )
-
-        return TaskStatus.SUCCESS
-
-    log.warning("Could not send all messages")
-
-    node_address, node_index = get_next_node_address(node_list, node_index)
-
-    if not node_address:
-        log.error(
-            f"This is the last node in the list, stopping. Only sent {idx} messages"
-        )
-        return TaskStatus.FAILED
-
-    log.info(f"Redirecting task to {node_address} (#{node_index} - {api_host})")
-    app.send_task(
-        f"{envvar('TASK_NAME')}.{node_address}",
-        args=(peer_id, count - idx, node_list, node_index),
-        queue=node_address,
-    )
-
-    return TaskStatus.SPLITTED
+    return status
