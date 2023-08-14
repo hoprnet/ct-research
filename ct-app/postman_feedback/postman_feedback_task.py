d17415c0ba4a612a2ed1fe4ed4d08274430b7b51
Jean Demeusy <jdemeusy@me.com>
2023-08-14 16:45:24 +0200

    renamed task and vars

--- ct-app/postman_feedback/postman_feedback_task.py
+++ ct-app/postman_feedback/postman_feedback_task.py
@@ -3,13 +3,15 @@ from tools import envvar, getlogger
 
 from tools.db_connection import DatabaseConnection
 
+
 _db_columns = [
     ("id", "SERIAL PRIMARY KEY"),
     ("peer_id", "VARCHAR(255) NOT NULL"),
     ("node_address", "VARCHAR(255) NOT NULL"),
     ("count", "INTEGER NOT NULL"),
     ("timestamp", "TIMESTAMP NOT NULL DEFAULT NOW()"),
-]
+]  # will be moved to DatabaseConnection attributes
+_table_name = "rewardTable"  # will be moved to DatabaseConnection attributes
 
 
 log = getlogger()
@@ -22,10 +24,11 @@ app = Celery(
 
 
 # the name of the task is the name of the "<task_name>"
-@app.task(name="foo_task")
-def foo_task(peer_id: str, node_address: str, count: int):
+@app.task(name="feedback_task")
+def feedback_task(peer_id: str, node_address: str, count: int):
     """
-    Celery task to ..."""
+    Celery task to store the message delivery status in the database.
+    """
 
     with DatabaseConnection(
         envvar("DB_NAME"),
@@ -35,14 +38,14 @@ def foo_task(peer_id: str, node_address: str, count: int):
         envvar("DB_PORT", int),
     ) as db:
         try:
-            db.create_table("rewardTable", _db_columns)
+            db.create_table(_table_name, _db_columns)
         except ValueError as e:
             log.warning(f"Error creating table: {e}")
 
-        if not db.table_exists_guard("raw_data_table"):
+        if not db.table_exists_guard(_table_name):
             log.error("Table not available, not sending to DB")
             return
 
-        db.insert(
-            "rewardTable", peer_id=peer_id, node_address=node_address, count=count
-        )
+        db.insert(_table_name, peer_id=peer_id, node_address=node_address, count=count)
+        # TODO: once everything merged to dev, here a check should be added to see if
+        # insertion is successful
