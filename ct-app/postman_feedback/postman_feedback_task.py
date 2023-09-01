19e65925a4b59b7dc1d48a4a0731499d1f8a1723
Jean Demeusy <jdemeusy@me.com>
2023-09-01 18:41:42 +0200

    use the new way to connect to the db

--- ct-app/postman_feedback/postman_feedback_task.py
+++ ct-app/postman_feedback/postman_feedback_task.py
@@ -1,19 +1,7 @@
 from celery import Celery
 from tools import envvar, getlogger
 
-from tools.db_connection import DatabaseConnection
-
-
-_db_columns = [
-    ("id", "SERIAL PRIMARY KEY"),
-    ("peer_id", "VARCHAR(255) NOT NULL"),
-    ("node_address", "VARCHAR(255) NOT NULL"),
-    ("expected_count", "INTEGER NOT NULL"),
-    ("effective_count", "INTEGER NOT NULL"),
-    ("status", "VARCHAR(255) NOT NULL"),
-    ("timestamp", "FLOAT NOT NULL"),
-]  # will be moved to DatabaseConnection attributes
-_table_name = "rewardTable"  # will be moved to DatabaseConnection attributes
+from tools.db_connection import DatabaseConnection, Reward
 
 
 log = getlogger()
@@ -21,7 +9,6 @@ log = getlogger()
 app = Celery(
     name=envvar("PROJECT_NAME"),
     broker=f"amqp://{envvar('RABBITMQ_USERNAME')}:{envvar('RABBITMQ_PASSWORD')}@{envvar('RABBITMQ_HOST')}/{envvar('RABBITMQ_VIRTUALHOST')}",
-    # backend=envvar("CELERY_RESULT_BACKEND"),
 )
 
 
@@ -39,24 +26,8 @@ def feedback_task(
     Celery task to store the message delivery status in the database.
     """
 
-    with DatabaseConnection(
-        envvar("DB_NAME"),
-        envvar("DB_HOST"),
-        envvar("DB_USER"),
-        envvar("DB_PASSWORD"),
-        envvar("DB_PORT", int),
-    ) as db:
-        try:
-            db.create_table(_table_name, _db_columns)
-        except ValueError as e:
-            log.warning(f"Error creating table: {e}")
-
-        if not db.table_exists_guard(_table_name):
-            log.error("Table not available, not sending to DB")
-            return
-
-        db.insert(
-            _table_name,
+    with DatabaseConnection() as session:
+        entry = Reward(
             peer_id=peer_id,
             node_address=node_address,
             expected_count=expected_count,
@@ -64,5 +35,8 @@ def feedback_task(
             status=status,
             timestamp=timestamp,
         )
-        # TODO: once everything merged to dev, here a check should be added to see if
-        # insertion is successful
+
+        session.add(entry)
+        session.commit()
+
+        log.debug(f"Stored reward entry in database: {entry}")
