fd1670b4f6b25e4ec8627b16415c9275565e8f90
Jean Demeusy <jdemeusy@me.com>
2023-09-06 20:35:33 +0200

    fix

--- ct-app/postman/postman_tasks.py
+++ ct-app/postman/postman_tasks.py
@@ -23,10 +23,7 @@ class TaskStatus(Enum):
     FAILED = "FAILED"
 
 
-app = Celery(
-    name=envvar("PROJECT_NAME"),
-    broker=f"amqp://{envvar('RABBITMQ_USERNAME')}:{envvar('RABBITMQ_PASSWORD')}@{envvar('RABBITMQ_HOST')}/{envvar('RABBITMQ_VIRTUALHOST')}",
-)
+app = Celery(name=envvar("PROJECT_NAME"), broker=envvar("CELERY_BROKER_URL"))
 app.autodiscover_tasks(force=True)
 
 
