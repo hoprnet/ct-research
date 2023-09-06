3eb54410e1b77269662574ac01fa36e2592f61b3
Jean Demeusy <jdemeusy@me.com>
2023-09-06 13:56:47 +0200

    duplicate in task names

--- ct-app/postman/postman_tasks.py
+++ ct-app/postman/postman_tasks.py
@@ -182,7 +182,7 @@ async def async_send_1_hop_message(
     return status, feedback_status
 
 
-@app.task(name=envvar("TASK_NAME"))
+@app.task(name="fake_task")
 def fake_task(
     peer: str,
     expected_count: int,
