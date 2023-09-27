59f168d05697b7ae500e91e016d0c18751751934
Jean Demeusy <jdemeusy@me.com>
2023-09-27 12:29:18 +0200

    automatic test discovery

--- ct-app/tests_endurance/__init__.py
+++ ct-app/tests_endurance/__init__.py
@@ -1,5 +1,32 @@
+import importlib
+import os
+
 from .endurance_test import EnduranceTest
 from .metric import Metric
-from .test_send_messages import SendMessages
 
-__all__ = ["EnduranceTest", "Metric", "SendMessages"]
+__all__ = ["EnduranceTest", "Metric"]
+
+
+def filename_to_classname(filename: str):
+    """Convert filename to corresponding classname."""
+    parts = filename.split("_")[1:]
+    classname = "".join([part.capitalize() for part in parts])
+    return classname
+
+
+# List all the .py files in the current directory that start with 'test_'
+module_files = [
+    f.strip(".py")
+    for f in os.listdir(os.path.dirname(os.path.abspath(__file__)))
+    if f.startswith("test_") and f.endswith(".py")
+]
+
+# Import the corresponding class from each module
+for module_file in module_files:
+    module = importlib.import_module(f".{module_file}", package=__name__)
+    class_name = filename_to_classname(module_file)
+
+    # Import the class to the current module's namespace
+    globals()[class_name] = getattr(module, class_name)
+
+    __all__.append(class_name)
