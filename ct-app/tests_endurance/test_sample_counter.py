d833aae860d6aef32671ece89006bf344d0a1e1f
Jean Demeusy <jdemeusy@me.com>
2023-09-27 18:34:01 +0200

    replace sampe test by something working

---
+++ ct-app/tests_endurance/test_sample_counter.py
@@ -0,0 +1,24 @@
+import logging
+
+from tools import getlogger
+
+from . import EnduranceTest, Metric
+
+log = getlogger()
+log.setLevel(logging.ERROR)
+
+
+class SampleCounter(EnduranceTest):
+    async def on_start(self):
+        self.counter = 0
+
+    async def task(self) -> bool:
+        self.counter += 1
+
+    async def on_end(self):
+        pass
+
+    def metrics(self):
+        self.metric_list = [
+            Metric("Counter", self.counter, cdt=f"== {self.duration * self.rate}")
+        ]
