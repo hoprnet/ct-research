48b72c33fb04437277e537c7ce15013ce0c193f1
Jean Demeusy <jdemeusy@me.com>
2023-11-03 17:23:06 +0100

    re-added tests to workflow, removed old files

--- ct-app/aggregator/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import middlewares, routes, setup, utils
-from .aggregator import Aggregator
-
-__all__ = ["Aggregator", "middlewares", "routes", "utils", "setup"]
--- ct-app/netwatcher/__init__.py
+++
@@ -1,5 +0,0 @@
-from .netwatcher import NetWatcher
-from .address import Address
-from .peer import Peer
-
-__all__ = ["NetWatcher", "Address", "Peer"]
