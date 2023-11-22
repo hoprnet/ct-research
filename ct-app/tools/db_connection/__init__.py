d8a5a78e265d924a3cb4ccee66bd70d6b1d3b2ad
Jean Demeusy <jdemeusy@me.com>
2023-11-22 10:35:57 +0100

    removed old 'ct-app' folder and renamed new one to 'ct-app'

--- ct-app/tools/db_connection/__init__.py
+++
@@ -1,9 +0,0 @@
-from .database_connection import DatabaseConnection
-from .models import Base, NodePeerConnection, Reward
-
-__all__ = [
-    "DatabaseConnection",
-    "Base",
-    "NodePeerConnection",
-    "Reward",
-]
