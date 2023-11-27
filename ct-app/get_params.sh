3a61748a976bf5b76b2ff0494993fec7dbbb7d53
Jean Demeusy <jdemeusy@me.com>
2023-11-27 17:43:17 +0100

    Refactor get_params.sh script to retrieve and process variables from multiple files

--- ct-app/get_params.sh
+++ ct-app/get_params.sh
@@ -1,14 +1,34 @@
-file_path="./core/node.py"
 
-if [ -z "$file_path" ]; then
-    echo "Usage: $0 <file>"
+folder_path=$1
+
+if [ -z "$folder_path" ]; then
+    echo "Usage: $0 <folder>"
     exit 1
 fi
 
-# Use grep to find lines containing "params" or "self.params" at the beginning of a word
-# -o option only prints the matched parts of a line
-variable_list=$(grep -oE '\b(self\.)?params[a-zA-Z0-9_]*' "$file_path")
+all_variables=""
 
-# Print the list of variables
-echo "List of variables:"
-echo "$variable_list"
+
+# Use find to get a list of files in the specified folder
+for file in "$folder_path"/*; do
+    if [ -f "$file" ]; then        
+        # Use grep to find lines containing "params" or "self.params" at the beginning of a word
+        # -o option only prints the matched parts of a line
+        variable_list=$(grep -oE '\b(self\.)?params[[:alnum:]._]*\b' "$file" | awk '{sub(/(self\.)?params/, ""); sub(/^\.+/, ""); if ($0 != "") print}')
+
+        all_variables+="$variable_list"
+    fi
+done
+
+unique_variables=$(echo "$all_variables" | tr ' ' '\n' | sort -u)
+
+IFS=$'\n' # Set Internal Field Separator to newline to iterate over lines
+
+for variable in $unique_variables; do
+    # Apply your own logic here
+    # Example: Print the variable with some custom message
+
+    var="$(echo $variable | tr a-z A-Z | tr . _)"
+
+    echo "$var"
+done
