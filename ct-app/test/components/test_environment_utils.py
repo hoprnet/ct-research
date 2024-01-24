18b0730f1ce29e27c929bf3f5298c82da1e1f8ab
Jean Demeusy <jdemeusy@me.com>
2024-01-24 12:09:56 +0100

    fix test environment in CI

--- ct-app/test/components/test_environment_utils.py
+++ ct-app/test/components/test_environment_utils.py
@@ -1,66 +1,61 @@
 import os
+from contextlib import contextmanager
 from pathlib import Path
 
 from core.components.utils import EnvironmentUtils
 
 
+@contextmanager
+def handle_envvars(**kwargs):
+    for key, value in kwargs.items():
+        os.environ[key.upper()] = value
+
+    try:
+        yield
+    finally:
+        for key in kwargs.keys():
+            del os.environ[key.upper()]
+
+
 def test_envvar():
-    os.environ["STRING_ENVVAR"] = "string-envvar"
-    os.environ["INT_ENVVAR"] = "1"
-    os.environ["FLOAT_ENVVAR"] = "1.0"
-
-    assert EnvironmentUtils.envvar("FAKE_STRING_ENVVAR", "default") == "default"
-    assert EnvironmentUtils.envvar("STRING_ENVVAR", type=str) == "string-envvar"
-    assert EnvironmentUtils.envvar("INT_ENVVAR", type=int) == 1
-    assert EnvironmentUtils.envvar("FLOAT_ENVVAR", type=float) == 1.0
-
-    del os.environ["STRING_ENVVAR"]
-    del os.environ["INT_ENVVAR"]
-    del os.environ["FLOAT_ENVVAR"]
+    with handle_envvars(
+        string_envvar="string-envvar", int_envvar="1", float_envvar="1.0"
+    ):
+        assert EnvironmentUtils.envvar("FAKE_STRING_ENVVAR", "default") == "default"
+        assert EnvironmentUtils.envvar("STRING_ENVVAR", type=str) == "string-envvar"
+        assert EnvironmentUtils.envvar("INT_ENVVAR", type=int) == 1
+        assert EnvironmentUtils.envvar("FLOAT_ENVVAR", type=float) == 1.0
 
 
 def test_envvarWithPrefix():
-    os.environ["TEST_ENVVAR_2"] = "2"
-    os.environ["TEST_ENVVAR_1"] = "1"
-    os.environ["TEST_ENVVAR_3"] = "3"
-    os.environ["TEST_ENVVOR_4"] = "3"
-
-    assert EnvironmentUtils.envvarWithPrefix("TEST_ENVVAR_", type=int) == {
-        "TEST_ENVVAR_1": 1,
-        "TEST_ENVVAR_2": 2,
-        "TEST_ENVVAR_3": 3,
-    }
-
-    del os.environ["TEST_ENVVAR_1"]
-    del os.environ["TEST_ENVVAR_2"]
-    del os.environ["TEST_ENVVAR_3"]
-    del os.environ["TEST_ENVVOR_4"]
+    with handle_envvars(
+        test_envvar_2="2",
+        test_envvar_1="1",
+        test_envvar_3="3",
+        test_envvor_4="4",
+    ):
+        assert EnvironmentUtils.envvarWithPrefix("TEST_ENVVAR_") == {
+            "TEST_ENVVAR_1": "1",
+            "TEST_ENVVAR_2": "2",
+            "TEST_ENVVAR_3": "3",
+        }
+        assert EnvironmentUtils.envvarWithPrefix("TEST_ENVVAR_", type=int) == {
+            "TEST_ENVVAR_1": 1,
+            "TEST_ENVVAR_2": 2,
+            "TEST_ENVVAR_3": 3,
+        }
 
 
 def test_checkRequiredEnvVar():
-    test_folder = Path(__file__).parent.joinpath("test_code")
-    file = test_folder.joinpath("test_main.py")
-    test_folder.mkdir(exist_ok=False)
-    file.touch(exist_ok=False)
-
-    file.write_text(
-        """
-        var1 = params.group1.var1
-        var2 = params.group1.var2
-        var3 = params.group2.var1
-        var4 = params.var1
-        """
-    )
+    test_folder = Path(__file__).parent.joinpath("test_code_for_environment")
 
     assert not EnvironmentUtils.checkRequiredEnvVar(test_folder)
 
-    os.environ["GROUP1_VAR1"] = "val11"
-    os.environ["GROUP1_VAR2"] = "val12"
-    os.environ["GROUP2_VAR1"] = "val21"
-    os.environ["VAR1"] = "val1"
-
-    assert EnvironmentUtils.checkRequiredEnvVar(test_folder)
-
-    # cleanup
-    file.unlink()
-    test_folder.rmdir()
+    with handle_envvars(
+        group1_var1="val11",
+        group1_var2="val12",
+        group2_var1="val21",
+        var1="val1",
+        var2="val2",
+    ):
+        assert EnvironmentUtils.checkRequiredEnvVar(test_folder)
