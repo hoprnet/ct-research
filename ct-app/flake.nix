56d7bb59a7c9f4e61105b5f3dee2ab96595da26a
Jean Demeusy <jdemeusy@me.com>
2024-03-07 17:22:25 +0100

    setup nix, fixed the list of parameters script

---
+++ ct-app/flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in rec {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          python39
+          python39Packages.venvShellHook
+        ];
+
+        venvDir = "./env";
+        postVenvCreation = ''
+          unset SOURCE_DATE_EPOCH
+          pip install -U pip setuptools wheel
+          pip install -r requirements_dev.txt
+        '' + pkgs.lib.optionalString pkgs.stdenv.isLinux ''
+          autoPatchelf ./.venv
+        '';
+
+      };
+    }
+  );
+}
