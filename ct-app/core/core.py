9fa87f3430975d1aad98560e2f7def7bfee244dc
Jean Demeusy <jdemeusy@me.com>
2024-05-27 17:32:45 +0200

    Add error handling for missing ticket price

--- ct-app/core/core.py
+++ ct-app/core/core.py
@@ -461,6 +461,10 @@ class Core(Base):
         """
         price = await self.api.ticket_price()
 
+        if price is None:
+            self.warning("Ticket price not available.")
+            return
+        
         await self.ticket_price.set(price)
         self.debug(f"Ticket price: {price}")
 
