5d9b8fe4bc4a7aa5412910129d7c8e923dded526
Jean Demeusy <61140535+jeandemeusy@users.noreply.github.com>
2024-08-23 09:45:13 +0200

    use unclaimed amount instead of claimed

--- ct-app/core/model/subgraph/entries/allocation_entry.py
+++ ct-app/core/model/subgraph/entries/allocation_entry.py
@@ -5,9 +5,13 @@ from .safe_entry import SafeEntry
 class AllocationEntry(SubgraphEntry):
     def __init__(self, id: str, claimedAmount: str, allocatedAmount: str):
         self.address = id
-        self.claimedAmount = float(claimedAmount) / 1e18
-        self.allocatedAmount = float(allocatedAmount) / 1e18
+        self.claimed_amount = float(claimedAmount) / 1e18
+        self.allocated_amount = float(allocatedAmount) / 1e18
         self.linked_safes: set[SafeEntry] = set()
+
+    @property
+    def unclaimed_amount(self):
+        return self.allocated_amount - self.claimed_amount
 
     @property
     def num_linked_safes(self):
