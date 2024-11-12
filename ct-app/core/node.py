c118de4f9345a0b100998a8b16e699eac601527f
Jean Demeusy <61140535+jeandemeusy@users.noreply.github.com>
2024-11-12 15:32:36 +0100

    use both peers and channels

--- ct-app/core/node.py
+++ ct-app/core/node.py
@@ -427,11 +427,13 @@ class Node(Base):
     async def watch_message_queue(self):
         message: MessageFormat = await MessageQueue().buffer.get()
 
-        destination_peers = [
-            channel.destination_peer_id for channel in self.channels.outgoing
-        ]
+        peers = [peer.address.id for peer in await self.peers.get()]
+        if message.relayer not in peers:
+            self.warning(f"Peer {message.relayer} not reachable")
+            return
 
-        if message.relayer not in destination_peers:
+        channels = [channel.destination_peer_id for channel in self.channels.outgoing]
+        if message.relayer not in channels:
             self.warning(f"No channel to {message.relayer}")
             return
 
