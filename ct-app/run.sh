c8c389179ac5d96a0e487fc2f85fdb2ca0a28d55
Jean Demeusy <61140535+jeandemeusy@users.noreply.github.com>
2024-11-15 17:12:09 +0100

    parameter enhancement

--- ct-app/run.sh
+++ ct-app/run.sh
@@ -3,22 +3,24 @@ export $(grep -v '^#' .env | xargs)
 
 env=${1:-staging}
 count=${2:-1}
-deployment=${3:-blue}
+deployment=${3:-auto}
 
 
 healthyz() { echo $(curl -s -o /dev/null -w "%{http_code}" "$1/healthyz"); }
 
-# check_deployment() {
-#     if [ $(healthyz $(printf $1 "blue" "1" $2)) -eq 200 ]; then
-#         echo "blue"
-#         exit 1
-#     elif [ $(healthyz $(printf $1 "green" "1" $2)) -eq 200 ]; then
-#         echo "green"
-#         exit 1
-#     fi
-# }
+check_deployment() {
+    if [ $(healthyz $(printf $1 "blue" "1" $2)) -eq 200 ]; then
+        echo "blue"
+        exit 1
+    elif [ $(healthyz $(printf $1 "green" "1" $2)) -eq 200 ]; then
+        echo "green"
+        exit 1
+    fi
+}
 
-# deployment=$(check_deployment $HOST_FORMAT $env)
+if [ $deployment == "auto" ]; then
+    deployment=$(check_deployment $HOST_FORMAT $env)
+fi
 
 # Node parameters
 for i in $(seq 1 $count); do
