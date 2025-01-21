06b5e9d409e0811a0d4361c585c71f8d945d47d6
Jean Demeusy <61140535+jeandemeusy@users.noreply.github.com>
2025-01-21 14:29:08 +0100

    class ordering

--- ct-app/core/components/messages.py
+++ ct-app/core/components/messages.py
@@ -6,6 +6,21 @@ from .singleton import Singleton
 
 QUEUE_SIZE = Gauge("ct_queue_size", "Size of the message queue")
 
+
+class MessageFormat:
+    def __init__(self, relayer: str, size: int):
+        if size < 0:
+            raise ValueError("Size must be a positive integer")
+        self.relayer = relayer
+        self.size = size
+
+    @property
+    def bytes(self):
+        message_as_bytes = self.relayer.encode()
+        if len(message_as_bytes) > self.size:
+            raise ValueError("Encoded relayer length exceeds specified size")
+        return message_as_bytes + b"\0" * (self.size - len(message_as_bytes))
+
 class MessageQueue(metaclass=Singleton):
     def __init__(self):
         self._buffer = Queue()
@@ -26,16 +41,3 @@ class MessageQueue(metaclass=Singleton):
             instance._buffer.get_nowait()
             instance._buffer.task_done()
             
-class MessageFormat:
-    def __init__(self, relayer: str, size: int):
-        if size < 0:
-            raise ValueError("Size must be a positive integer")
-        self.relayer = relayer
-        self.size = size
-
-    @property
-    def bytes(self):
-        message_as_bytes = self.relayer.encode()
-        if len(message_as_bytes) > self.size:
-            raise ValueError("Encoded relayer length exceeds specified size")
-        return message_as_bytes + b"\0" * (self.size - len(message_as_bytes))
