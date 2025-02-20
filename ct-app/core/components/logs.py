fed65d95aa39215ee2129c75e97658add2accd7e
Jean Demeusy <61140535+jeandemeusy@users.noreply.github.com>
2025-02-20 14:21:07 +0100

    adapt format

--- ct-app/core/components/logs.py
+++ ct-app/core/components/logs.py
@@ -1,34 +1,28 @@
 import json
 import logging
-import platform
 import sys
 import time
 import traceback
 
 
 class JSONFormatter:
-    def __init__(self, **tags):
-        self.tags = tags
-
+    def __init__(self):
+        pass
+    
     def format(self, record):
         timestamp = time.strftime("%Y-%m-%dT%H:%M:%S", time.gmtime(record.created)) + (".%03dZ" % (1000 * (record.created % 1)))
 
         result = {
             'timestamp':        timestamp,
-            'severity':         record.levelname,
-            'logger':           record.name,
+            'level':            record.levelname,
             'message':          record.msg % record.args,
-            'host':             platform.node(),
-            'process_id':       record.process,
-            'thread':           record.threadName,
-            'location_info': {
-                'filename':     record.filename,
-                'line_number':  record.lineno
+            'threadId':           record.threadName,
+            'fields': {
+                'target': record.name,
+                'log_filename': record.filename,
+                'log_line_number': record.lineno,
             }
         }
-
-        if self.tags:
-            result['tags'] = self.tags
 
         if (record.exc_info):
             result['exception'] = traceback.format_exception(
