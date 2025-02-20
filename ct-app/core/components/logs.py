97c8db410873593f82c21b53f065fc41480249aa
Jean Demeusy <61140535+jeandemeusy@users.noreply.github.com>
2025-02-20 12:22:41 +0100

    format

--- ct-app/core/components/logs.py
+++ ct-app/core/components/logs.py
@@ -7,27 +7,23 @@ import traceback
 
 
 class JSONFormatter:
-    """A formatter for the standard logging module that converts a LogRecord into JSON
-    Output matches JSONLayout from https://github.com/kdgregory/log4j-aws-appenders. Any
-    keyword arguments supplied to the constructor are output in a "tags" sub-object.
-    """
-
     def __init__(self, **tags):
         self.tags = tags
 
     def format(self, record):
+        timestamp = time.strftime("%Y-%m-%dT%H:%M:%S", time.gmtime(record.created)) + (".%03dZ" % (1000 * (record.created % 1)))
+
         result = {
-            'timestamp':    time.strftime("%Y-%m-%dT%H:%M:%S", time.gmtime(record.created)) +
-            (".%03dZ" % (1000 * (record.created % 1))),
-            'severity':        record.levelname,
-            'logger':       record.name,
-            'message':      record.msg % record.args,
-            'host':     platform.node(),
-            'process_id':    record.process,
-            'thread':       record.threadName,
+            'timestamp':        timestamp,
+            'severity':         record.levelname,
+            'logger':           record.name,
+            'message':          record.msg % record.args,
+            'host':             platform.node(),
+            'process_id':       record.process,
+            'thread':           record.threadName,
             'location_info': {
                 'filename':     record.filename,
-                'line_number':   record.lineno
+                'line_number':  record.lineno
             }
         }
 
