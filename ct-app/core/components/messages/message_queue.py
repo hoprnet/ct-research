0a5b2c2d268304dea3e5e2e2ea080ed9b75fc5aa
Jean Demeusy <61140535+jeandemeusy@users.noreply.github.com>
2025-02-25 16:35:32 +0100

    increase load

--- ct-app/core/components/messages/message_queue.py
+++ ct-app/core/components/messages/message_queue.py
@@ -1,4 +1,4 @@
-import janus
+from janus import Queue
 from prometheus_client import Gauge
 
 from ..singleton import Singleton
@@ -9,27 +9,30 @@ QUEUE_SIZE = Gauge("ct_queue_size", "Size of the message queue")
 
 class MessageQueue(metaclass=Singleton):
     def __init__(self):
-        self._buffer = janus.Queue()
+        self._buffer = Queue()
 
     def get_sync(self) -> MessageFormat:
         return self.buffer.sync_q.get()
 
-    async def get_async(self) -> MessageFormat:
-        return await self.buffer.async_q.get()
-
     def put_sync(self, item: MessageFormat):
         self.buffer.sync_q.put(item)
+
+    async def get_async(self) -> MessageFormat:
+        return await self.buffer.async_q.get()
 
     async def put_async(self, item: MessageFormat):
         self.buffer.async_q.put(item)
 
     @property
+    def size(self):
+        return self._buffer.sync_q.qsize()
+
+    @property
     def buffer(self):
-        QUEUE_SIZE.set(self._buffer._qsize())
+        QUEUE_SIZE.set(self.size)
         return self._buffer
 
     @classmethod
     def clear(cls):
-        while not cls()._buffer.empty():
-            cls()._buffer.get_nowait()
-            cls()._buffer.task_done()
+        while not cls().size:
+            cls().get_sync()
