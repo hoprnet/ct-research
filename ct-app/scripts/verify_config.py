b65f34c7518282a4455bcf3cbb010ebeaedcdc5e
Jean Demeusy <61140535+jeandemeusy@users.noreply.github.com>
2025-05-13 15:36:23 +0200

    multiple files as arg in verify script

--- ct-app/scripts/verify_config.py
+++ ct-app/scripts/verify_config.py
@@ -9,24 +9,29 @@ from core.components.config_parser import Parameters
 
 
 @click.command()
-@click.option("--file", "-f", type=str, help="Input config file")
-def main(file: str):
+@click.option("--file", "-f", type=str, multiple=True, help="Input config file")
+def main(file: list[str]):
+    result = []
+    for f in file:
+        with open(f, "r") as stream_file:
+            data = yaml.safe_load(stream_file)
 
-    with open(file, "r") as f:
-        data = yaml.safe_load(f)
-
-    try:
-        _ = Parameters.verify(data)
-    except KeyError as e:
-        click.echo(f"KeyError in {file}: {e}")
-        return 1
-    except ValueError as e:
-        click.echo(f"ValueError in {file}: {e}")
-        return 1
+        try:
+            _ = Parameters.verify(data)
+        except KeyError as e:
+            click.echo(f"KeyError in {f}: {e}")
+            result.append(1)
+        except ValueError as e:
+            click.echo(f"ValueError in {f}: {e}")
+            result.append(1)
+        else:
+            result.append(0)
+    if any(result):
+        click.echo("Some config files are invalid.")
+        sys.exit(1)
     else:
-        click.echo(f"Config {file} is valid.")
-        return 0
-
+        click.echo("All config files are valid.")
+        sys.exit(0)
 
 if __name__ == "__main__":
     main()
