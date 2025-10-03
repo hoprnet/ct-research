7006cfc6cb3e0cc51dfd8ad3339f07113fadab81
Jean Demeusy <61140535+jeandemeusy@users.noreply.github.com>
2025-10-03 11:09:19 +0200

    feat: increase prod batch window (#694)

--- ct-app/test/api/test_balance.py
+++ ct-app/test/api/test_balance.py
@@ -6,15 +6,15 @@ from core.components.balance import Balance
 
 
 def test_parse_string_to_balance():
-    balance_str = "100.00 HOPR"
+    balance_str = "100.00 wxHOPR"
     balance = Balance(balance_str)
 
     assert balance.value == 100.00
-    assert balance.unit == "HOPR"
+    assert balance.unit == "wxHOPR"
 
 
 def test_parse_string_to_balance_invalid_format():
-    balance_str = "100.00 wei HOPR but not valid"
+    balance_str = "100.00 wei wxHOPR but not valid"
 
     with pytest.raises(TypeError):
         _ = Balance(balance_str)
@@ -28,9 +28,9 @@ def test_parse_string_to_balance_empty():
 
 
 def test_compare_balances():
-    balance1 = Balance("100.00 HOPR")
-    balance2 = Balance("100.00 HOPR")
-    balance3 = Balance("50.00 HOPR")
+    balance1 = Balance("100.00 wxHOPR")
+    balance2 = Balance("100.00 wxHOPR")
+    balance3 = Balance("50.00 wxHOPR")
 
     assert balance1 == balance2
     assert balance1 != balance3
@@ -39,22 +39,22 @@ def test_compare_balances():
 
 
 def test_add_balances():
-    balance1 = Balance("100.00 HOPR")
-    balance2 = Balance("50.00 HOPR")
-    balance3 = Balance("10000000000000000 wei HOPR")
+    balance1 = Balance("100.00 wxHOPR")
+    balance2 = Balance("50.00 wxHOPR")
+    balance3 = Balance("10000000000000000 wei wxHOPR")
 
     result = balance1 + balance2
 
     assert result.value == 150.00
-    assert result.unit == "HOPR"
+    assert result.unit == "wxHOPR"
 
     result = balance1 + balance3
-    assert result.value == Decimal("100.01")  # 100.00 HOPR + 0.01 HOPR in wei
+    assert result.value == Decimal("100.01")  # 100.00 wxHOPR + 0.01 wxHOPR in wei
 
 
 def test_add_balances_edge_case():
-    balance1 = Balance("0.01 HOPR")
-    balance2 = Balance("0.02 HOPR")
+    balance1 = Balance("0.01 wxHOPR")
+    balance2 = Balance("0.02 wxHOPR")
 
     result = balance1 + balance2
 
@@ -62,37 +62,37 @@ def test_add_balances_edge_case():
 
 
 def test_sub_balances():
-    balance1 = Balance("100.00 HOPR")
-    balance2 = Balance("50.00 HOPR")
+    balance1 = Balance("100.00 wxHOPR")
+    balance2 = Balance("50.00 wxHOPR")
 
     result = balance1 - balance2
 
     assert result.value == 50.00
-    assert result.unit == "HOPR"
+    assert result.unit == "wxHOPR"
 
 
 def test_sub_balances_with_conversion():
-    balance1 = Balance("100.00 HOPR")
-    balance2 = Balance("10000000000000000 wei HOPR")
+    balance1 = Balance("100.00 wxHOPR")
+    balance2 = Balance("10000000000000000 wei wxHOPR")
 
     result = balance1 - balance2
 
     assert result.value == Decimal("99.99")
-    assert result.unit == "HOPR"
+    assert result.unit == "wxHOPR"
 
 
 def test_div_balances():
-    balance1 = Balance("100.50 HOPR")
+    balance1 = Balance("100.50 wxHOPR")
 
     result = balance1 / 2
 
     assert result.value == 50.25
-    assert result.unit == "HOPR"
+    assert result.unit == "wxHOPR"
 
 
 def test_div_balance_with_balance():
-    balance1 = Balance("100.00 HOPR")
-    balance2 = Balance("50.00 HOPR")
+    balance1 = Balance("100.00 wxHOPR")
+    balance2 = Balance("50.00 wxHOPR")
 
     result = balance1 / balance2
 
